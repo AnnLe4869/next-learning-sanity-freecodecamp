a9349b3530d4b9cb60ab3ad1ad628250d8994b60
Anh Le <kunquan2345@gmail.com>
2021-07-05 02:57:55 -0700

    Add chef schema
    _ In a schema the type is the most important as it dictates what properties are needed
      In our case, the type that need attentions are: document, reference and array
    _ document is the gate: every other types is nested under the property fields of the type document
      Think of document type as <form> and other type is <input>
    _ array is a set of other type. Exception is the block type since it require parent type to be array

---
+++ nextlearningsanityfreecodecamp/schemas/chef.ts
@@ -0,0 +1,42 @@
+const chefSchema = {
+  name: "chef",
+  title: "Chef",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Chef's name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "bio",
+      title: "Bio",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [
+            { title: "Normal", value: "normal" },
+            { title: "H1", value: "h1" },
+            { title: "H2", value: "h2" },
+            { title: "H3", value: "h3" },
+            { title: "H4", value: "h4" },
+            { title: "Quote", value: "blockquote" },
+          ],
+          lists: [{ title: "Bullet", value: "bullet" }],
+        },
+      ],
+    },
+  ],
+};
+
+export default chefSchema;
